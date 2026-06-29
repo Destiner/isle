f0cd844769b704b4f456acc042196e5d4353ca5b
Timur Badretdinov <4247901+Destiner@users.noreply.github.com>
2026-06-29 21:12:31 +0100

    Initial Commit

---
+++ IsleTests/IsleTests.swift
@@ -0,0 +1,19 @@
+//
+//  IsleTests.swift
+//  IsleTests
+//
+//  Created by Timur Badretdinov on 29/06/2026.
+//
+
+import Testing
+@testable import Isle
+
+struct IsleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
